e84dce5a71e333832349e0fcf1a3d6fec462d3cd
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2024-11-20 09:41:21 -0300

    criando mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
 import express from "express";
 
+const posts = [
+  {
+   id: 1, 
+   descricao: "Foto teste" ,
+   imagem: "https://placecats.com/300/150" 
+  },
+  { 
+    id: 2,
+    descricao: "Gato fofo dormindo", 
+    imagem: "https://placecats.com/200/200" 
+  },
+  { 
+    id: 3,
+    descricao: "Paisagem incrível", 
+    imagem: "https://picsum.photos/200/300" 
+  },
+  {
+    id: 4,
+    descricao: "Cachorro brincando",
+    imagem: "https://placeimg.com/300/200/animals",
+  },
+  {
+    id: 5,
+    descricao: "Comida deliciosa",
+    imagem: "https://loremflickr.com/320/240/food",
+  },
+  {
+    id: 6,
+    descricao: "Viagem inesquecível",
+    imagem: "https://source.unsplash.com/random",
+  },
+];
+
 const app = express();
-app.listen(3000, ()=>{
-    console.log("servidor escutando na porta 3000");
+app.use(express.json());
+app.listen(3000, () => {
+  console.log("servidor escutando na porta 3000");
 });
 
-app.get("/api", (req, res)=> {
-    res.status(200).send('Boas vindas a imersão');
+function buscarPostPorId(id) {
+    return posts.findIndex((post) => 
+        post.id === Number(id)
+    );
+}
 
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorId(req.params.id);
+  res.status(200).json(posts[index]);
 });
